34703f513217e57d7bb0ed5a64eecd0ae6302611
Rafael Schreiber <33909431+rafaelschreiber@users.noreply.github.com>
2018-05-10 17:56:35 +0200

    Update fish_prompt.fish

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -2,7 +2,11 @@ function fish_prompt
 	set error_code $status
 	set git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
 	set_color -o white
-	echo ""
+	if test $history[1] = "clear"
+		echo -n ""
+	else
+		echo ""
+	end
 	echo -n "╔ "
 	if test $error_code -eq 0
 		set_color -o green
