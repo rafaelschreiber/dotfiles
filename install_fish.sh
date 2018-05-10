e4a5acdc6aa864616b9f993bf54a21437e452595
Rafael Schreiber <33909431+rafaelschreiber@users.noreply.github.com>
2018-05-10 17:09:17 +0200

    Added Install Script

---
+++ install_fish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm $HOME/.config/fish/functions/fish_prompt.fish
+rm $HOME/.config/fish/functions/fish_right_prompt.fish
+rm $HOME/.config/fish/functions/fish_greeting.fish
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cp $SCRIPTPATH/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
+cp $SCRIPTPATH/fish_right_prompt.fish $HOME/.config/fish/functions/fish_right_prompt.fish
+cp $SCRIPTPATH/fish_greeting.fish $HOME/.config/fish/functions/fish_greeting.fish
