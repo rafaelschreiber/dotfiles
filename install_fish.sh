2603d05c9c3009b0907336f8499f0508cd2829e6
Rafael Schreiber <33909431+rafaelschreiber@users.noreply.github.com>
2018-05-13 15:04:11 +0200

    Update install_fish.sh

--- install_fish.sh
+++ install_fish.sh
@@ -4,6 +4,7 @@ rm $HOME/.config/fish/functions/fish_right_prompt.fish
 rm $HOME/.config/fish/functions/fish_greeting.fish
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+mkdir $HOME/.config/fish/functions
 
 cp $SCRIPTPATH/fish/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
 cp $SCRIPTPATH/fish/fish_right_prompt.fish $HOME/.config/fish/functions/fish_right_prompt.fish
