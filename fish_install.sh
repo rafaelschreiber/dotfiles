9a270be0dff24c103f10733f028c9b00fe972723
Rafael Schreiber <raf.schreiber@gmail.com>
2019-06-01 11:44:42 +0200

    added fish colorscheme

--- fish_install.sh
+++ fish_install.sh
@@ -3,6 +3,7 @@ mv $HOME/.config/fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fi
 mv $HOME/.config/fish/functions/fish_right_prompt.fish $HOME/.config/fish/functions/fish_right_prompt.fish.bak 2>/dev/null
 mv $HOME/.config/fish/functions/fish_greeting.fish $HOME/.config/fish/functions/fish_greeting.fish.bak 2>/dev/null
 mv $HOME/.config/fish/functions/sudo.fish $HOME/.config/fish/functions/sudo.fish.bak 2>/dev/null
+mv $HOME/.config/fish/fish_variables $HOME/.config/fish/fish_variables.bak 2>/dev/null
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p $HOME/.config/fish/functions
@@ -11,3 +12,4 @@ cp $SCRIPTPATH/dot_fish/fish_prompt.fish $HOME/.config/fish/functions/fish_promp
 cp $SCRIPTPATH/dot_fish/fish_right_prompt.fish $HOME/.config/fish/functions/fish_right_prompt.fish
 cp $SCRIPTPATH/dot_fish/fish_greeting.fish $HOME/.config/fish/functions/fish_greeting.fish
 cp $SCRIPTPATH/dot_fish/sudo.fish $HOME/.config/fish/functions/sudo.fish
+cp $SCRIPTPATH/dot_fish/fish_variables $HOME/.config/fish/fish_variables
